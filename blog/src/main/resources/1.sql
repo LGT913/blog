00c4d7dadc214283f0630f052d4bd15be9cbef51
L G T <17262854+xiaolingexia@user.noreply.gitee.com>
2026-07-27 20:12:00 +0800

    chore: 清理冗余文件
    - 删除 1.sql 测试数据文件

--- blog/src/main/resources/1.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO site_config (config_key, config_value, description, create_time, update_time)
-VALUES (
-           'blog_info',
-           '{"siteName":"我的个人博客","siteDesc":"技术学习与分享的小站","copyright":"©2026 个人博客 版权所有"}',
-           '博客基础配置信息',
-           NOW(),
-           NOW()
-       );
